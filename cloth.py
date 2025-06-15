7f9021cc6c7413503d86d36e9497db869c54eac8
Satyajit Shil <satyajitshil02@gmail.com>
2025-06-15 13:03:21 -0700

    rgb(107, 135, 15) x=rgb(12, 55, 25) print(x)

---
+++ cloth.py
@@ -0,0 +1,7 @@
+s = input("WHat is the season: ")
+if s == "Summer":
+    b = input("How hot is it")
+    if b == "Hot":
+        print("Wear light clothes")
+else:
+    print("Wear clothes that will keep you warm")
